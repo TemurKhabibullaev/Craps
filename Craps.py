25ae3a021322774ee92fc2ca69185ce9be28bd0b
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2019-10-23 13:11:18 -0400

    Update Craps.py

--- Craps.py
+++ Craps.py
@@ -4,8 +4,6 @@ import random
 import time
 
 # Balance
-
-## According to the instructions, this should be a function definition to get the initial bankroll.
 print('''
 Welcome to Craps Game!
 You will be asked the full amount of money from your bankroll.
@@ -18,41 +16,55 @@ balance = int(input('> '))
 print("Good! Let's start! Place how much you bet!")
 bet = int(input('> '))
 
+total_amount = balance - bet
+
 
 def finish():
     print('Good Job! Game is finished. Here is your balance:\n> ', total_amount)
 
-## SHould not define a function inside a loop.  You can call the function in the loop if needed.
+
+def rollDice():
+    print('Rolling in the deep...')
+    time.sleep(3)
+    points = random.randint(2,12)
+    print('This is your final points: ',points)
+    return points
+
+
+def win():
+    won_money = total_amount + bet * 2
+    print('Wow! You won! Your win is: ',won_money)
+
+
+def loosing():
+    lost_money = total_amount - bet * 2
+    print('You lose. This is your lose amount:', lost_money)
+
+roll = rollDice()
 while bet <= balance:
-    def rollDice():
-        total_amount = balance - bet
-
-        first_roll = random.randint(1,6)
-        print('Rolling in the deep...')
+    if int(roll) == 7 or int(roll) == 11:
         time.sleep(3)
-        print('This is your first roll: ',first_roll)
-
-        second_roll = random.randint(1,6)
-        print('Rolling in the deep...')
+        win()
+    elif int(roll) == 2 or int(roll) == 3 or int(roll) == 12:
         time.sleep(3)
-        print('This is your second roll: ',second_roll)
-        time.sleep(3)
-        points = first_roll + second_roll
-        print('And this is your final points: ',points)
-        if points == 7 or points == 11:
-            won_money = total_amount + bet * 2
-            time.sleep(3)
-            print('Wow! You won! Your balance is: ', won_money)
-        elif points == 2 or points == 3 or points == 12:
-            lost_money = total_amount - bet * 2
-            time.sleep(3)
-            print('You lost. This is your current balance: ', lost_money)
-        else:
-            print("You didn't loose nor win.")
-            print('This is your balance now: ',total_amount)
-    decision = input('Are we playing? Know- wins or loses will be cut from your balance. Enter an INTEGER only:\n1) Yes\n2) No\n> ')
+        loosing()
+    elif roll == 4 or roll == 5 or roll == 6 or roll == 8 or roll == 9 or roll == 10:
+        print(f'Your points: {roll}. You will keep rolling until you get same points (Win) or 7 (Lose).')
+        print('This is your balance now: ',total_amount)
+        new_roll = random.randint(2,12)
+        while roll != new_roll and new_roll != 7:
+            print(f"You have rolled a {new_roll}, we'll keep going.")
+            time.sleep(2)
+            new_roll = random.randint(2,12)
+        if new_roll == roll:
+            print(f"You got {new_roll}!")
+            win()
+        elif new_roll == 7:
+            print(f"You got {new_roll}")
+            loosing()
+    decision = input('Are we playing? Know - wins or loses will be cut from your balance. Enter an INTEGER only:\n1) Yes\n2) No\n> ')
     if int(decision) == 1:
         rollDice()
     elif int(decision) != 1:
-        print('Good Luck!')
+        print('Good Luck! See you next time.')
         break
